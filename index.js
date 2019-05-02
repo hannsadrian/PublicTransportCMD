4f63502459adb274a28e891accfa5789b99d7691
Adwirawien <hannsadrian@gmail.com>
2019-05-02 16:37:13 +0200

    Complete function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const inquirer = require("inquirer");
 const colors = require("colors");
+const fetch = require("node-fetch");
 
 async function getStopFromUser() {
   return new Promise((resolve, reject) => {
@@ -19,16 +20,81 @@ async function getStopFromUser() {
   });
 }
 
-async function getDepartures() {
+async function getStop(hst) {
+  return new Promise((resolve, reject) => {
+    fetch(
+      "http://widgets.vvo-online.de/abfahrtsmonitor/Haltestelle.do?hst=" + hst
+    ).then(res => resolve(res.json()));
+  });
+}
+
+async function getDepartures(hst) {
+  return new Promise((resolve, reject) => {
+    fetch(
+      "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?hst=" + hst
+    ).then(res => resolve(res.json()));
+  });
+}
+
+async function executeCMD() {
+  console.log("");
   console.log(colors.bold(colors.america("-------------------------")));
+  console.log("");
   var stop = await getStopFromUser();
-  console.log(colors.bold(colors.america("-------------------------")));
+  console.log("");
   console.log(
-    colors.italic(
-      colors.bold(colors.green("+")) + " Fetching Departures for "
-    ) + colors.italic(colors.cyan(stop))
+    colors.italic(colors.yellow("-") + " Fetching Departures for ") +
+      colors.italic(colors.cyan(stop))
   );
+  var departures = await getDepartures(stop);
+  var stopFound = await getStop(stop);
+  console.log(
+    colors.italic(colors.yellow("-") + " Found departures for ") +
+      colors.italic(
+        colors.cyan(JSON.stringify(stopFound[1][0][0]).replace(/"/g, ""))
+      )
+  );
+  console.log("");
+  var max = [0, 0, 3];
+  departures.forEach(departure => {
+    if (departure[0].length > max[0]) max[0] = departure[0].length;
+    if (departure[1].length > max[1]) max[1] = departure[1].length;
+    if (departure[2].length > max[2]) max[2] = departure[2].length;
+  });
+  var header = ["Linie", "Min"];
+  for (i = 0; i < max[0] + max[1] + 1; i++) {
+    if (header[0].length < max[0] + max[1] + 1) header[0] += " ";
+  }
+  console.log(
+    colors.gray("  | ") +
+      colors.white(header[0]) +
+      colors.gray(" | ") +
+      colors.white(header[1]) +
+      colors.gray(" |")
+  );
+  departures.forEach(departure => {
+    for (i = 0; i < departure.length; i++) {
+      for (y = 0; y < max[i]; y++) {
+        if (departure[i].length < max[i]) {
+          departure[i] += " ";
+        }
+      }
+    }
+
+    if (departure[2] === "   ") departure[2] = "0  ";
+
+    console.log(
+      colors.gray("  | ") +
+        colors.yellow(departure[0]) +
+        colors.gray(" ") +
+        colors.white(departure[1]) +
+        colors.gray(" | ") +
+        colors.white(departure[2]) +
+        colors.gray(" |")
+    );
+  });
+  console.log("");
   console.log(colors.bold(colors.america("-------------------------")));
 }
 
-getDepartures();
+executeCMD();
